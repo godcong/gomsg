1c152beb761a608507b54ebda329a40e8b9006b8
godcong <jumbycc@163.com>
2022-06-22 16:34:00 +0800

    feat(monitor): add monitor

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/godcong/gomsg
+
+go 1.18
+
+require (
+	github.com/orcaman/concurrent-map/v2 v2.0.0
+	github.com/panjf2000/ants/v2 v2.5.0
+)
---
+++ monitor.go
@@ -0,0 +1,51 @@
+package gomsg
+
+import (
+	cmap "github.com/orcaman/concurrent-map/v2"
+	ants "github.com/panjf2000/ants/v2"
+)
+
+type MsgFn = func(key string, val interface{})
+
+type monitors struct {
+	size int
+	msg  cmap.ConcurrentMap[[]MsgFn]
+	pool *ants.Pool
+}
+
+type Monitor interface {
+	WaitFor(key string, fn MsgFn)
+	Send(key string, data interface{})
+}
+
+func New(size int) *monitors {
+	pool, _ := ants.NewPool(size)
+	cmap.SHARD_COUNT = size
+	m := monitors{
+		size: size,
+		msg:  cmap.New[[]MsgFn](),
+		pool: pool,
+	}
+	return &m
+}
+
+func (m *monitors) WaitFor(key string, fn MsgFn) {
+	s := []MsgFn{fn}
+	v, exist := m.msg.Get(key)
+	if exist {
+		v = append(s, v...)
+	}
+	m.msg.Set(key, s)
+}
+
+func (m *monitors) Send(key string, data interface{}) {
+	v, exist := m.msg.Get(key)
+	if !exist {
+		return
+	}
+	for i := range v {
+		_ = m.pool.Submit(func() {
+			v[i](key, data)
+		})
+	}
+}
